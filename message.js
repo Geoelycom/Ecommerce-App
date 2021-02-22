a3f9a137a11e75de8670c1d854054da9a2acc2b6
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-02-22 10:37:35 +0100

    setting up the workflow for today

---
+++ message.js
@@ -0,0 +1,9 @@
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    const input = document.querySelector('#message-input');
+
+
+    const encrypted = btoa(input.value);
+    document.querySelector('#link-input').value = encrypted;
+})
