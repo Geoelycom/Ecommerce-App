9bc337bce8b8b6b0281d566c083c96048ba473fe
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-02-25 03:40:14 +0100

    final changes to app

--- watchit/watchit.js
+++ watchit/watchit.js
@@ -1,8 +1,39 @@
 #!/usr/bin/env node
 
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
-chokidar
-    .watch('.')
-    .on('add', () => console.log('FILE ADDED'))
-    .on('change', () => console.log('FILE CHANGED'))
-    .on('unlink', () => console.log('FILE UNLINKED'));
+const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
+
+program
+    .version('1.4.0')
+    .argument('[filename]', 'Name of file to execute')
+    .action(async({ filename }) => {
+        const name = filename || 'watchit.js';
+
+        try {
+            await fs.promises.access(name);
+        } catch (err) {
+            throw new Error(`could not find the file ${name}`);
+        }
+
+
+
+        let proc;
+        const start = debounce(() => {
+
+            if (proc) {
+                proc.kill();
+            }
+            proc = spawn('node', [name], { stdio: 'inherit' });
+        }, 100);
+
+        chokidar
+            .watch('.')
+            .on('add', start)
+            .on('change', start)
+            .on('unlink', start);
+    });
+
+program.parse(process.argv);
