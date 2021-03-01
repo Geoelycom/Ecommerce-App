330cc72163eaef1567234e480d8bf0f35cd5f0d8
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-03-01 16:59:07 +0100

    customise database for both app and test basis

--- watchit/eccomerce app/ecommerce.js
+++ watchit/eccomerce app/ecommerce.js
@@ -1,8 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const usersRepo = require('./repository/users');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.send('hi there!');
+    res.send(`
+        <div>
+           <form method ="POST">
+                <input name ="email" placeholder ="email" />
+                <input name ="password" placeholder ="password" />
+                <input name ="passwordConfirmation" placeholder ="password confirmation" />
+                
+                <button>sign up </button>
+        </form>
+     </div>
+    
+    
+    `);
+});
+app.post('/', async(req, res) => {
+    const { email, password, passwordConfirmation } = req.body;
+    const existingUser = await usersRepo.getOneBy({ email });
+    if (existingUser) {
+        return res.send('Email in use');
+    }
+    if (password !== passwordConfirmation) {
+        return res.send('passwords must match')
+    }
+    res.send('account created')
+
 });
 app.listen(3000, () => {
     console.log('listening')
---
+++ watchit/eccomerce app/repository/users.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const crpto = require('crypto');
+
+class UsersRepository {
+    constructor(filename) {
+        if (!filename) {
+            throw new Error('to crate a new file require a filename');
+        }
+
+        this.filename = filename;
+        try {
+            fs.accessSync(this.filename);
+        } catch (err) {
+            fs.writeFileSync(this.filename, '[]')
+        }
+
+    }
+
+    async getAll() {
+        return JSON.parse(
+            await fs.promises.readFile(this.filename, {
+                encoding: 'utf8'
+            })
+        );
+    }
+    async create(attr) {
+        attr.id = this.randomId();
+        const records = await this.getAll();
+        records.push(attr);
+    }
+    async writeAll(records) {
+        await fs.promises.writeFile(
+            this.filename,
+            JSON.stringify(records, null, 2)
+        );
+    }
+    randomId() {
+        return crpto.randomBytes(4).toString('hex');
+    }
+
+    async getOne(id) {
+        const records = await this.getAll();
+        return records.find(record => record.id === id);
+    }
+
+    async delete(id) {
+        const records = await this.getAll();
+        const filteredRecords = records.filter(record => record.id !== id);
+        await this.writeAll(filteredRecords);
+    }
+
+    async update(attr, id) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+        if (!record) {
+            throw new Error(`Record with id${id} not found`)
+        }
+        Object.assign(record, attr);
+        await this.writeAll(records);
+    }
+    async getOneBy(filters) {
+        const records = await this.getAll();
+        for (let record of records) {
+            let found = true;
+            for (let key in filters) {
+                if (record[key] !== filters[key]) {
+                    found = false
+                }
+            }
+            if (found) {
+                return record;
+
+            }
+        }
+    }
+
+
+}
+
+module.exports = new UsersRepository('users.json');
