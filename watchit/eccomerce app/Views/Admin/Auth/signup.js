9a7b869eb3cebd52f3af4dc82162987a72b3c99c
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-03-05 23:59:07 +0100

    setting up the html template for signup route

---
+++ watchit/eccomerce app/Views/Admin/Auth/signup.js
@@ -0,0 +1,17 @@
+module.exports = ({ req }) => {
+    return `
+    <div>
+        your id is : ${req.session.userId}
+           <form method ="POST">
+                <input name ="email" placeholder ="email" />
+                <input name ="password" placeholder ="password" />
+                <input name ="passwordConfirmation" placeholder ="password confirmation" />
+                
+                <button>sign up </button>
+        </form>
+     </div>
+    
+    
+    
+    `
+}
