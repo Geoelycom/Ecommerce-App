aec54c11b4ad686579ac72c0b6b2b656570ff83e
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-03-10 22:52:45 +0100

    creating the products html form

---
+++ watchit/eccomerce app/Views/Admin/products/new.js
@@ -0,0 +1,18 @@
+const layout = require('../layout');
+
+const { getError } = require('../../helpers')
+
+
+module.exports = ({ errors }) => {
+    return layout({
+        content: `
+        <form method = "POST">
+        <input placeholder ="Title" name="title" />
+        <input placeholder="Price" name="price" />
+        <input type="file" name="image" />
+        <button>Submit</button>
+        </form>
+        
+    `
+    })
+}
--- watchit/eccomerce app/routes/Admin/product.js
+++ watchit/eccomerce app/routes/Admin/product.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const produtsRepo = require('../../repository/products');
+const productsNewTemplate = require('../../Views/Admin/products/new');
+
 
 const router = express.Router();
 
@@ -8,6 +10,7 @@ router.get('/admin/product', (req, res) => {
 });
 
 router.get('/admin/product/new', (req, res) => {
+    res.send(productsNewTemplate({}));
 
 })
 
