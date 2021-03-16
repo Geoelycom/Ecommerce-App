5a6c5d64bd0bd35606ebab0ab6d16c2442f0949b
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-03-17 00:15:51 +0100

    creating a cart for a user

--- watchit/eccomerce app/routes/carts.js
+++ watchit/eccomerce app/routes/carts.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts')
+
 
 const router = express.Router();
 
 // revcieve a post request to add an item to a cart
-router.post('/carts/products', (req, res) => {
+router.post('/cart/products', async(req, res) => {
+    let cart;
+    if (!req.session.cartId) {
 
+
+
+
+        cart = await cartsRepo.create({ items: [] });
+        req.session.cartId = cart.id;
+    } else {
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+
+    console.log(cart);
+
+
+
+    res.send('Product added to cart')
 })
-
 
 // recieve a GET request to show all items in a cart
 
