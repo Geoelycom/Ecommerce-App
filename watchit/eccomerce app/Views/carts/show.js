0b8db3b567db7710b25ced42e5948b4ecff67d44
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-03-17 02:27:12 +0100

    addingthe delete functionality to shopping cart

--- watchit/eccomerce app/Views/carts/show.js
+++ watchit/eccomerce app/Views/carts/show.js
@@ -1,6 +1,10 @@
 const layout = require('../layout');
 
 module.exports = ({ items }) => {
+    const totalPrice = items.reduce((prev, item) => {
+        return prev + item.quantity * item.product.price;
+    }, 0)
+
     const renderedItems = items
         .map(item => {
             return `
@@ -14,7 +18,8 @@ module.exports = ({ items }) => {
               $${item.product.price * item.quantity}
             </div>
             <div class="remove">
-              <form method="POST">
+              <form method="POST" action="/cart/products/delete">
+              <input hidden value="${item.id}" name="itemId" />
                 <button class="button is-danger">                  
                   <span class="icon is-small">
                     <i class="fas fa-times"></i>
@@ -42,7 +47,7 @@ module.exports = ({ items }) => {
               <div class="message-header">
                 Total
               </div>
-              <h1 class="title">$</h1>
+              <h1 class="title">$${totalPrice}</h1>
               <button class="button is-primary">Buy</button>
             </div>
           </div>
--- watchit/eccomerce app/routes/carts.js
+++ watchit/eccomerce app/routes/carts.js
@@ -32,7 +32,7 @@ router.post('/cart/products', async(req, res) => {
     })
 
 
-    res.send('Product added to cart')
+    res.redirect('/cart')
 })
 
 // recieve a GET request to show all items in a cart
@@ -47,11 +47,18 @@ router.get('/cart', async(req, res) => {
         item.product = product;
 
     }
+
     res.send(cartShowTemplate({ items: cart.items }))
 
+});
+router.post('/cart/products/delete', async(req, res) => {
+    const { itemId } = req.body;
+    const cart = await cartsRepo.getOne(req.session.cartId);
+    const items = cart.items.filter(item => item.id !== itemId);
+    await cartsRepo.update(req.session.cartId, { items });
+    res.redirect('/cart')
 })
 
 
 // recieve a post request to delete an item from a cart
-
 module.exports = router;
